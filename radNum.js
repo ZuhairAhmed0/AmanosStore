1a0c64d26e61a3ee3d43d5f6a4feda219f6053f8
ZuhairAhmed0 <zuh12390@gmail.com>
2023-09-14 11:27:45 +0000

    add README.md and last updates of project

--- radNum.js
+++
@@ -1,8 +0,0 @@
-const randomCard = [];
-const crypto = require("crypto");
-
-for (let i = 0; i < 8; i++) {
-    const code = crypto.randomBytes(7).toString("hex");
-    randomCard.push(code)
-}
-console.log(randomCard.join(","));
